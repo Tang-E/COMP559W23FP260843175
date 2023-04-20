abda6dd53460e0be0de59e7b06d2ca5533f6d9e6
Betangent <rwa_edwin@hotmail.com>
2023-04-19 21:34:18 -0400

    Manually added imgui files into source directory subdirectory imgui/.

--- COMP559W23FP260843175/src/Application.cpp
+++ COMP559W23FP260843175/src/Application.cpp
@@ -13,9 +13,9 @@
 #include "OpenGlHelpers.h"
 #include "Scene.h"
 #include "FlipFluid.cpp"
-#include <imgui.h>
-#include <imgui_impl_glfw.h>
-#include <imgui_impl_opengl3.h>
+#include "imgui/imgui.h"
+#include "imgui/imgui_impl_glfw.h"
+#include "imgui/imgui_impl_opengl3.h"
 
 
 
